3b0f56294232b0fa0f045c5d39bc2e64c37dcf3d
prakash-pradik <dontgiveupfreddie@gmail.com>
2024-09-21 17:23:32 +0530

    updated base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/leestore_office/';
+$config['base_url'] = 'http://office.leestoreindia.com/';
 
 date_default_timezone_set('Asia/Kolkata'); 
 /*
