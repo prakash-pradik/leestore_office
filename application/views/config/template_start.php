3eae0a9cfa92256ac98576bae4a5ed24e90db922
prakash-pradik <dontgiveupfreddie@gmail.com>
2024-10-15 09:28:18 +0530

    Reverted office folders

--- application/views/config/template_start.php
+++ application/views/config/template_start.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
 
-        <title>Lee Store - POS</title>
+        <title>Lee Store Admin</title>
 
         <meta name="description" content="Lee Store Admin Panel">
         <meta name="author" content="Prakash Freddie">
@@ -25,20 +25,19 @@
 
         <!-- The main stylesheet of this template. All Bootstrap overwrites are defined in here -->
         <link rel="stylesheet" href="<?php echo base_url(CSS); ?>/main.css">
-        <link rel="stylesheet" href="<?php echo base_url(CSS); ?>/custom.css">
 
         <!-- Include a specific file here from css/themes/ folder to alter the default theme of the template -->
 
         <!-- The themes stylesheet of this template (for using specific theme color in individual elements - must included last) -->
         <link rel="stylesheet" href="<?php echo base_url(CSS); ?>/themes.css">
+
         <link rel="stylesheet" href="<?php echo base_url(CSS); ?>/themes/night.css">
         <!-- END Stylesheets -->
 
         <!-- Modernizr (browser feature detection library) -->
         <script src="<?php echo base_url(JS); ?>/vendor/modernizr.min.js"></script>
 
-        <link rel="stylesheet" href="<?php echo base_url(CSS); ?>/sweetalert2.min.css"/>
-        <link rel="stylesheet" href="<?php echo base_url(CSS); ?>/alertify.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
     </head>
     <body>
-    <input type="hidden" id="base_url" value="<?php echo base_url(); ?>">
--- office/README.md
+++
@@ -1,1 +0,0 @@
-# leestore_office
